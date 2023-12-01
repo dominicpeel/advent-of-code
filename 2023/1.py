a613ff02f6452040d7c67d92db9036810030c69f
Dominic Peel <dominic.peel@ocado.com>
2023-12-01 22:00:41 +0000

    day 1 initial solution

---
+++ 2023/1.py
@@ -0,0 +1,39 @@
+words = "one, two, three, four, five, six, seven, eight, nine".split(", ")
+p1 = p2 = 0
+
+f = open("inp1").readlines()
+for line in f:
+    parsed = ""
+    for i, ch in enumerate(line):
+        if ch.isdigit():
+            parsed += ch
+        else:
+            for n, word in enumerate(words):
+                j = 0
+                while j < len(word) and i+j < len(line):
+                    if line[i+j] == word[j]:
+                        j += 1
+                    else:
+                        break
+                if j == len(word):
+                    parsed += str(n+1)
+
+    first, last = None, None
+    for ch in line:
+        if ch.isdigit():
+            if first == None:
+                first = ch
+            last = ch
+    resA += int(first+last)
+
+    first, last = None, None
+    for ch in parsed:
+        if ch.isdigit():
+            if first == None:
+                first = ch
+            last = ch
+    resB += int(first+last)
+
+print("a", p1)
+print("b", p2)
+
