5bbf5f13897bc87664641d2fa304100c3d698886
Dominic Peel <dominic@peely.org>
2023-12-24 23:39:50 +0000

    day 24 part 1

---
+++ 2023/24.py
@@ -0,0 +1,35 @@
+hailstones = [[list(map(int, vector.split(", "))) for vector in line.split(" @ ")] for line in open("inp24").read().strip().split("\n")]
+
+bnd = [200000000000000, 400000000000000]
+#bnd = [7, 27]
+
+p1 = 0
+for i in range(len(hailstones)):
+    for j in range(i+1, len(hailstones)):
+        a_pos, a_vel = hailstones[i]
+        b_pos, b_vel = hailstones[j]
+
+        a_px, a_py, a_pz = a_pos
+        b_px, b_py, b_pz = b_pos
+
+        a_vx, a_vy, a_vz = a_vel
+        b_vx, b_vy, b_vz = b_vel
+
+
+        if b_vx * a_vy == b_vy * a_vx:
+            continue
+        
+        ta = ((b_py - a_py) * b_vx - (b_px - a_px) * b_vy) / (b_vx * a_vy - b_vy * a_vx)
+        tb = ((b_py - a_py) * a_vx - (b_px - a_px) * a_vy) / (b_vx * a_vy - b_vy * a_vx)
+
+        if ta<0 or tb < 0:
+            continue
+
+        px = a_px + a_vx*ta
+        py = a_py + a_vy*ta
+
+        if bnd[0] <= px <= bnd[1] and bnd[0] <= py <= bnd[1]:
+            p1 += 1
+
+print(p1)
+
